38073adb05b9025b4747bf1c76e726a91f5358a3
Mehul263 <75162693+Mehul263@users.noreply.github.com>
2021-01-26 11:13:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+var cartValue = document.getElementById("cart-value");
+var cartButton = document.getElementById("cart");
+
+var books1Add = document.getElementById("book1");
+var books2Add = document.getElementById("book2");
+var books3Add = document.getElementById("book3");
+var books4Add = document.getElementById("book4");
+var books5Add = document.getElementById("book5");
+var books6Add = document.getElementById("book6");
+
+var games1Add = document.getElementById("games1");
+var games2Add = document.getElementById("games2");
+var games3Add = document.getElementById("games3");
+var games4Add = document.getElementById("games4");
+
+var crafts1Add = document.getElementById("crafts1");
+var crafts2Add = document.getElementById("crafts2");
+var crafts3Add = document.getElementById("crafts3");
+var crafts4Add = document.getElementById("crafts4");
+
+console.log(books1Add);
